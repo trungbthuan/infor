c1c00d28758432cba7f2b603f50741715a4e6dab
Dinh Trung <trungbthuan@gmail.com>
2026-01-22 10:54:09 +0700

    Chuyen data tu SQLite3 sang PostgreSQL

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Nếu có PostgreSQL thì dùng, không thì dùng SQLite
 DATABASES = {
     'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        default=f'sqlite:///{os.path.join(BASE_DIR, "db.sqlite3")}',
         conn_max_age=600
     )
 }
