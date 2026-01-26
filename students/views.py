daa15b5d65827ebccc67441779710af87e53f193
Dinh Trung <trungbthuan@gmail.com>
2026-01-26 15:31:49 +0700

    Django tự lấy dường dẫn hiện tại

--- students/views.py
+++ students/views.py
@@ -6,44 +6,54 @@ import json
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
 
-@require_http_methods(["GET"]) # Chỉ cho phép method GET
+
+@require_http_methods(["GET"])  # Chỉ cho phép method GET
 def do_anything_view(request):
     return JsonResponse({'message': 'Đã chạy lệnh Python trên server.'})
+
 
 def call_Another_Template(request):
     return render(request, 'notification.html', {'message': 'Đã chạy lệnh Python trên server.'})
 
+
 def student_notification(request):
-    message="Test thử xem sao thôi mà"
-    return render(request, 'notification.html', {'message': message} )
+    message = "Test thử xem sao thôi mà"
+    return render(request, 'notification.html', {'message': message})
+
 
 def customConfirm(request):
     return render(request, 'customConfirm.html')
 
+
 def student_post(request):
     return render(request, 'student-post.html')
 
+
 @login_required
 def student_list(request):
-    api_url = 'http://localhost:8080/api/api-list-students/'
+    # api_url = 'http://localhost:8080/api/api-list-students/'
+    base_url = request.build_absolute_uri('/')[:-1]
+    api_url = f"{base_url}/api/api-list-students/"
     response = requests.get(api_url)
-    students_data = response.json() 
+    students_data = response.json()
     print(students_data)
     for student in students_data:
         dt = datetime.strptime(student['birthday'], "%d/%m/%Y")
         student['birthday'] = dt.date()
-    
+
     return render(request, 'student-list.html', {'students': students_data})
+
 
 @login_required
 def student_update(request):
-    api_url = 'http://localhost:8080/api/api-list-students/'
-    
+    # api_url = 'http://localhost:8080/api/api-list-students/'
+    base_url = request.build_absolute_uri('/')[:-1]
+    api_url = f"{base_url}/api/api-list-students/"
     try:
-        response = requests.get(api_url, params=request.GET) 
+        response = requests.get(api_url, params=request.GET)
         if response.status_code == 200:
             try:
-                students_data = response.json() 
+                students_data = response.json()
                 for student in students_data:
                     dt = datetime.strptime(student['birthday'], "%d/%m/%Y")
                     student['birthday'] = dt.date()
@@ -57,9 +67,12 @@ def student_update(request):
         error_message = f'Lỗi kết nối API: {e}'
         return render(request, 'api/notification.html', {'message': error_message})
 
+
 @login_required
-def student_edit(request, id): # Gọi form student-edit.html
-    api_url = f'http://localhost:8080/api/api-get-students-by-id/{id}/'
+def student_edit(request, id):  # Gọi form student-edit.html
+    # api_url = f'http://localhost:8080/api/api-get-students-by-id/{id}/'
+    base_url = request.build_absolute_uri('/')[:-1]
+    api_url = f"{base_url}/api/api-get-students-by-id/{id}/"
     try:
         response = requests.get(api_url)
         if response.status_code == 200:
@@ -74,14 +87,17 @@ def student_edit(request, id): # Gọi form student-edit.html
         error_message = f'Lỗi kết nối API: {e}'
         return render(request, 'notification.html', {'message': error_message})
 
-@login_required
-def student_put_by_id(request, id): # Xử lý CẬP NHẬT khi người dùng gửi từ form student-edit.html
 
-    api_url = f'http://localhost:8080/api/api-put-students/{id}/'
-    
+@login_required
+# Xử lý CẬP NHẬT khi người dùng gửi từ form student-edit.html
+def student_put_by_id(request, id):
+
+    # api_url = f'http://localhost:8080/api/api-put-students/{id}/'
+    base_url = request.build_absolute_uri('/')[:-1]
+    api_url = f"{base_url}/api/api-put-students/{id}/"
     if request.method == 'POST':
         # 2. Chuẩn bị dữ liệu từ form HTML (request.POST)
-        data_request = request.POST.dict() 
+        data_request = request.POST.dict()
 
         response = requests.put(api_url, data=data_request)
 
@@ -96,20 +112,22 @@ def student_put_by_id(request, id): # Xử lý CẬP NHẬT khi người dùng g
             error_message = f'Cập nhật thất bại. Chi tiết: {json.dumps(error_json, ensure_ascii=False)}'
             return render(request, 'notification.html', {'message': error_message})
 
+
 @login_required
 def student_delete_by_id(request, id):
-    api_url = f'http://localhost:8080/api/api-delete-students/{id}/'
-    
+    # api_url = f'http://localhost:8080/api/api-delete-students/{id}/'
+    base_url = request.build_absolute_uri('/')[:-1]
+    api_url = f"{base_url}/api/api-delete-students/{id}/"
     # 1. Gửi yêu cầu DELETE đến API (THAY vì requests.get)
-    response = requests.delete(api_url) 
-    
+    response = requests.delete(api_url)
+
     # 2. Kiểm tra mã trạng thái thành công 204
     if response.status_code == 204:
         # Nếu xóa thành công, CHUYỂN HƯỚNG về trang danh sách/cập nhật
-        return redirect('update') 
+        return redirect('update')
     else:
         error_message = f'Lỗi xóa sinh viên {id}. Mã trạng thái: {response.status_code}'
-        
+
         # Nếu API trả về JSON lỗi, bạn có thể cố gắng đọc nó
         try:
             error_details = response.json()
@@ -117,8 +135,5 @@ def student_delete_by_id(request, id):
         except:
             # Bỏ qua nếu không phải JSON
             pass
-            
-        return render(request, 'notification.html', {'message': error_message})
-    
-    
 
+        return render(request, 'notification.html', {'message': error_message})
